d50eb2620e4d2ab8289adef8045fdba67af305b0
Godwin-Coding <ohgodwin88@gmail.com>
2026-06-10 21:40:02 +0800

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stk = []
+        res = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            j = -1
+            term = -len(stk)
+            while j >= term:
+                if stk and temperatures[stk[j]] < temperatures[i]:
+                    res[stk[j]] = i-stk[j]
+                    stk.pop()
+                else:
+                    break
+            stk.append(i)
+            print(stk)
+        return res
